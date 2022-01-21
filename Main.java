bfee574008f78ed9350b8785f63637fd2a1547c0
Artem <artmartrus@rambler.ru>
2022-01-21 18:47:57 +0300

    fix 1

--- Main.java
+++ Main.java
@@ -1,11 +1,11 @@
-package Lambda.Worker;
+package lambda.worker;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        Worker.OnTaskDoneListener listener = System.out::println;
-        Worker.OnTaskErrorListener errorListener = System.out::println;
+        OnTaskDoneListener listener = System.out::println;
+        OnTaskErrorListener errorListener = System.out::println;
         Worker worker = new Worker(listener, errorListener);
         worker.start();
 
--- Worker.java
+++ Worker.java
@@ -1,19 +1,10 @@
-package Lambda.Worker;
+package lambda.worker;
 
 public class Worker {
 
     public Worker() {
     }
 
-    @FunctionalInterface
-    public interface OnTaskDoneListener {
-        void onDone(String result);
-    }
-
-    @FunctionalInterface
-    public interface OnTaskErrorListener{
-        void onError(String result);
-    }
 
     private OnTaskErrorListener errorCallback;
     private OnTaskDoneListener callback;
@@ -25,7 +16,7 @@ public class Worker {
 
 
     public void start() {
-        for (int i = 0; 100 <= i; i++)
+        for (int i = 0; i <= 100; i++)
             if (i != 33) {
                 callback.onDone("Task " + i + " is done");
             } else {
